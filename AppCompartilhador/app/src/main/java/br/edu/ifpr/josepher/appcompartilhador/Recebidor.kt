738f1bacf3246f9f23dc520b8d844d63dea6ff25
Josepher Castro <josephercastrosilva@gmail.com>
2020-07-10 23:44:14 -0300

    Recebe texto - OK

--- AppCompartilhador/app/src/main/java/br/edu/ifpr/josepher/appcompartilhador/Recebidor.kt
+++ AppCompartilhador/app/src/main/java/br/edu/ifpr/josepher/appcompartilhador/Recebidor.kt
@@ -1,11 +1,42 @@
 package br.edu.ifpr.josepher.appcompartilhador
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
+import kotlinx.android.synthetic.main.activity_recebidor.*
 
 class Recebidor : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recebidor)
+
+        when {
+            intent?.action == Intent.ACTION_SEND -> {
+                if ("text/plain" == intent.type) {
+                    handleSendText(intent) // Handle text being sent
+                } else if (intent.type?.startsWith("image/") == true) {
+                    handleSendImage(intent) // Handle single image being sent
+                }
+            }
+            else -> {
+                // Handle other intents, such as being started from the home screen
+            }
+        }
+    }
+
+    private fun handleSendImage(intent: Intent?) {
+        (intent?.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as? Uri)?.let {
+            // Update UI to reflect image being shared
+
+        }
+    }
+
+    private fun handleSendText(intent: Intent?) {
+        intent?.getStringExtra(Intent.EXTRA_TEXT)?.let {
+            // Update UI to reflect text being shared
+            txRecebe.text = it
+        }
     }
 }
